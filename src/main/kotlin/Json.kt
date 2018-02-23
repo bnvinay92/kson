e071583d3a4b8076b29cd2a971c12bc20ba2c35c
Vinay Nagaraj <vinay.nagaraj@olacabs.com>
2018-02-23 15:27:41 +0530

    Make code kidiomatic

--- src/main/kotlin/Json.kt
+++
@@ -1,8 +0,0 @@
-sealed class Json
-
-data class JBool(val boolean: Boolean) : Json()
-object JNull : Json ()
-data class JObject(val members : List<Pair<String, Json>>) : Json ()
-data class JArray(val elements: List<Json>) : Json ()
-data class JNumber(val double: Double) : Json ()
-data class JString(val string: String) : Json ()
--- src/main/kotlin/JsonParser.kt
+++ src/main/kotlin/JsonParser.kt
@@ -1,3 +1,12 @@
+sealed class Json
+
+data class JBool(val boolean: Boolean) : Json()
+object JNull : Json()
+data class JObject(val members: List<Pair<String, Json>>) : Json()
+data class JArray(val elements: List<Json>) : Json()
+data class JNumber(val double: Double) : Json()
+data class JString(val string: String) : Json()
+
 val space = one(' ')
 val spaces = many(space)
 
